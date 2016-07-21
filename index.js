1d091051bfa465b7ad04184dfdc6c9e43088c081
Zack Yang <zack9433@gmail.com>
2016-07-21 13:16:17 +0800

    fix(component): refactor

--- index.js
+++
@@ -1,9 +0,0 @@
-import 'angular-material.css';
-import 'angular-material-icons.css';
-import 'angular-material-data-table.css';
-
-import 'svg-morpheus';
-import 'angular';
-import component from './app/component';
-
-export default component
