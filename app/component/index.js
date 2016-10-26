87528348879ee752611d480192f4fcb6bef4666c
Zack Yang <zack9433@gmail.com>
2016-10-26 18:04:11 +0800

    fix(package): add offline status

--- app/component/index.js
+++ app/component/index.js
@@ -1,4 +1,7 @@
 // Vendors
+import 'offline-theme-chrome-indicator.css';
+import 'offline-language-english-indicator.css';
+import 'offline-js';
 import 'jquery';
 import 'jquery-sparkline';
 import webFont from 'webfontloader';
@@ -41,6 +44,21 @@ webFont.load({
     families: ['Roboto']
   }
 });
+
+Offline.options = {
+  // Should we check the connection status immediatly on page load.
+  checkOnLoad: true,
+  // Should we automatically retest periodically when the connection is down (set to false to disable).
+  reconnect: {
+    // How many seconds should we wait before rechecking.
+    initialDelay: 10,
+
+    // How long should we wait between retries.
+    delay: 10
+  },
+  // Should we store and attempt to remake requests which fail while the connection is down.
+  requests: false
+};
 
 const app = angular.module('sanji.core', [
   ngSanitize,
