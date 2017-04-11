c40333d5cc3fa5e1bb4992e2e2e2e9d5f48dd7c4
Zack Yang <zack9433@gmail.com>
2017-04-11 14:48:09 +0800

    fix(bug): console

--- src/component/helper/download.helper.js
+++ src/component/helper/download.helper.js
@@ -6,18 +6,13 @@ class DownloadHelperService {
   }
 
   start(url) {
-    console.log('===start iframe download helper====');
-    console.log('token', this.session.get('token'));
-    console.log('download link', url);
     this.generatedIframe(this.session.get('token'), url);
     this.$timeout(this.removeIframe.bind(this), 5000);
   }
 
   generatedIframe(token, url) {
     this.iframe = document.createElement('iframe');
-    console.log('create token cookies', token);
     this.$cookies.put('token', token);
-    console.log('created', this.$cookies.get('token'));
 
     this.iframe.src = url;
     this.iframe.style.display = 'none';
@@ -25,9 +20,7 @@ class DownloadHelperService {
   }
 
   removeIframe() {
-    console.log('remove token cookies');
     this.$cookies.remove('token');
-    console.log('removed', this.$cookies.get('token'));
     document.body.removeChild(this.iframe);
   }
 }
